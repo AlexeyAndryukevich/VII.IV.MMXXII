73ebaaf2cd49b521d1f5982e6e8b88e4b2ba2d1e
AndriukevychOleksii <oandriuk@softserveinc.com>
2021-04-21 04:36:31 +0300

    fix date

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,17 @@
+const countDownDate = new Date("Apr 7, 2022 14:00:00").getTime();
 
 const x = setInterval(function() {
     render();
 }, 10000);
 
 function render() {
-    const countDownDate = new Date("2022-04-07 14:00:00").getTime();
     const now = new Date().getTime();
     const distance = countDownDate - now;
     const days = Math.floor(distance / (1000 * 60 * 60 * 24));
     // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     // var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     // var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("countdown").innerHTML = countDownDate + '<br>' + now +  '<br>' + distance + '<br>' + days;
+    document.getElementById("countdown").innerHTML = days;
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("countdown").innerHTML = "Юхууу🥳";
